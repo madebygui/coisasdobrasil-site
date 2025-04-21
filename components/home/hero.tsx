268fabe4bee49c7d489a0443888c10990d9337fe
Guilherme Miranda <guilhermemiranda.ti@gmail.com>
2025-04-21 12:37:58 -0300

    Ajuste no hero

--- components/home/hero.tsx
+++ components/home/hero.tsx
@@ -1,9 +1,19 @@
+'use client';
+
+import Image from 'next/image';
 import './styles.css';
 
 export function Hero() {
   return (
     <section className='hero-slider' aria-label='Banner principal'>
       <div className='hero-image'>
+        <Image
+          src='/images/banner-home-1.webp'
+          alt='Banner linha infantil'
+          fill
+          priority
+          style={{ objectFit: 'cover' }}
+        />
         <div className='overlay'>
           <div className='text-container'>
             <h3 className='description'>LANÇAMENTO 2024 | Linha Infantil</h3>
